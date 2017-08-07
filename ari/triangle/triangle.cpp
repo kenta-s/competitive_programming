84064571aa1aef41cf9595e04d06f10f151a6cd7
kenta-s <knt01222@gmail.com>
2017-08-07 09:02:55 +0900

    Complete triangle

--- ari/triangle/triangle.cpp
+++ ari/triangle/triangle.cpp
@@ -1,26 +1,28 @@
 # include <iostream>
 
-int n = 5;
-int a[5] = {2, 3, 4, 5, 10};
-
-class Triangle {
-public:
-  void Power(){
-    for(int &x : a){
-      x = x * x;
-    }
-  }
-  void Pr(int num){
-    std::cout << num << std::endl;
-  }
-};
+// int n = 5;
+// int a[5] = {2, 3, 4, 5, 10};
+int n = 4;
+int a[4] = {4,5,10,20};
 
 int main() {
-  // Triangle tri;
-  // tri.Power();
-  // for(int x : a){
-  //   std::cout << x << std::endl;
-  // }
-  std::cout << std::max(2,std::max(3,7));
+  int ans = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if(i==j) continue;
+      for(int k = 0; k < n; k++){
+        if(i==k || j==k) continue;
+        int len = a[i] + a[j] + a[k];
+        int ma = std::max(std::max(a[i], a[j]), a[k]);
+        int rest = len - ma;
+        if(ma < rest) {
+          ans = std::max(ans, len);
+        }
+      }
+    }
+  }
+
+  std::cout << "answer is " << ans << std::endl;
+
   return 0;
 }
