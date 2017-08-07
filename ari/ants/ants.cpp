3d51aa6a0f51e5cc0e7071ac0999ee42b1bc2883
kenta-s <knt01222@gmail.com>
2017-08-07 13:39:40 +0900

    Add constructer to Ants

--- ari/ants/ants.cpp
+++ ari/ants/ants.cpp
@@ -2,16 +2,29 @@
 
 class Ants {
 public:
-  Ants();
+  Ants(){
+    L = 0;
+    n = 0;
+    min = L;
+    max = 0;
+  };
   int CalcMin(){
-    return 0;
+    for(int i = 0; i < n; i++){
+      min = std::min(min, x[i]);
+    }
+    return min;
   }
   int CalcMax(){
-    return 0;
+    for(int i = 0; i < n; i++){
+      max = std::max(max, x[i]);
+    }
+    return max;
   }
 private:
-  int L = 0;
-  int n = 0;
+  int L;
+  int n;
+  int min;
+  int max;
 };
 
 int main(){
@@ -19,7 +32,20 @@ int main(){
   int n = 3;
   int x[3] = {2, 6, 7};
 
-  Ants *ants = new Ants(1,3);
+  int min = L;
+  for(int i = 0; i < n; i ++){
+    min = std::min(L - x[i], min);
+  }
+  std::cout << "min is " << min << std::endl;
+
+  int max = 0;
+  for(int i = 0; i < n; i ++){
+    max = std::max(L - x[i], max);
+  }
+  std::cout << "max is " << max << std::endl;
+
+  Ants *ants = new Ants();
+  std::cout << ants->CalcMin() << std::endl;
 
   return 0;
 }
