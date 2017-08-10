e9f9b73c0073b072d8de1f111592b3ceb51a4b44
kenta-s <knt01222@gmail.com>
2017-08-10 18:08:06 +0900

    tmp

--- ari/lake_counting/lake_counting.cpp
+++ ari/lake_counting/lake_counting.cpp
@@ -5,7 +5,7 @@
 int N = 10;
 int M = 12;
 
-char field[10][13] = {
+const char *field[10][13] = {
   "W...........",
   ".W.........W",
   "..........WW",
@@ -22,7 +22,7 @@ void dfs(int x, int y){
   field[x][y] = ".";
   for(int dx = -1; dx <= 1; dx++){
     for(int dy = -1; dy <= 1; dy++){
-      if(field[dx][dy] == "W") dfs(dx, dy);
+      if(std::strcmp(field[dx][dy], "W")) dfs(dx, dy);
     }
   }
 }
@@ -31,7 +31,7 @@ int main(){
   int count = 0;
   for(int i = 0; i < N; i++){
     for(int j = 0; j < M; j++){
-      if(field[i][j] == "W"){
+      if(std::strcmp(field[i][j], "W")){
         dfs(i, j);
         count++;
       }
