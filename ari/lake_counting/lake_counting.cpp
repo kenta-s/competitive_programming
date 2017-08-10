5df4663c1fd6b836e6fac270d10e9dec1276e7ca
kenta-s <knt01222@gmail.com>
2017-08-10 13:41:29 +0900

    Update lake counting

--- ari/lake_counting/lake_counting.cpp
+++ ari/lake_counting/lake_counting.cpp
@@ -5,9 +5,38 @@
 int N = 10;
 int M = 12;
 
-char field[10][13];
+char field[10][13] = {
+  "W...........",
+  ".W.........W",
+  "..........WW",
+  ".........WWW",
+  "..........WW",
+  "..........WW",
+  "...W........",
+  ".WWWW.......",
+  "....W.......",
+  "............",
+};
+char fuga[3][2] = {
+  "2",
+  "2",
+  "2"
+};
+
+int dfs(int x, int y){
+  std::cout << x << std::endl;
+  if(x >= 10) return x;
+
+  return 0;
+}
 
 int main(){
+  char hoge[] = {"1234567890123"};
   std::cout << field << std::endl;
+  std::cout << hoge << std::endl;
+
+  for(int i = 0; i < N; i++){
+    std::cout << field[i] << std::endl;
+  }
   return 0;
 }
