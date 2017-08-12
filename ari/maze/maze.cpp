4871be457dbf32d454801ae266b6eb550a9c2f14
kenta-s <knt01222@gmail.com>
2017-08-12 19:10:36 +0900

    Complete Maze

--- ari/maze/maze.cpp
+++ ari/maze/maze.cpp
@@ -25,17 +25,15 @@ int dy[4] = {1, 0, -1, 0};
 
 int distance[10][11];
 
-void move(int x, int y){
-  distance[x][y] = 1;
+void move(int x, int y, int prevCount){
+  distance[x][y] = prevCount + 1;
   for(int i = 0; i < 4; i++){
     if(maze[x+dx[i]][y+dy[i]] == 'G'){
-      std::cout << "yessssssss!!!!!!!!!" << std::endl;
+      std::cout << "count is " << prevCount+1 << std::endl;
     }else if(maze[x+dx[i]][y+dy[i]] == '.'){
-      if(distance[x+dx[i]][y+dy[i]] == 1){
+      if(distance[x+dx[i]][y+dy[i]] > 0){
       }else{
-        std::cout << "x is " << x+dx[i] << std::endl;
-        std::cout << "y is " << y+dy[i] << std::endl;
-        move((x+dx[i]), (y+dy[i]));
+        move((x+dx[i]), (y+dy[i]), prevCount + 1);
       }
     }
   }
@@ -47,7 +45,7 @@ int main(){
   for(int i = 0; i < N; i++){
     for(int j = 0; j < M+1; j++){
       if(maze[i][j] == 'S' && count == 0){
-        move(i, j);
+        move(i, j, 0);
       }
     }
   }
