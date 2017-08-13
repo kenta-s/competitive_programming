79be5160d2d617ee59c4dd45fbc327f3bd6ac07f
kenta-s <knt01222@gmail.com>
2017-08-13 14:34:12 +0900

    Fix jobs

--- ari/jobs/jobs.cpp
+++ ari/jobs/jobs.cpp
@@ -1,15 +1,26 @@
 # include <iostream>
+# include <algorithm>
+
+const int MAX_N = 100000;
 
 int n = 5;
-int s[] = {1, 2, 4, 6, 8};
-int t[] = {3, 5, 7, 9, 10};
+int s[MAX_N] = {1, 2, 4, 6, 8};
+int t[MAX_N] = {3, 5, 7, 9, 10};
 
 void solve(){
+  std::pair<int, int> itv[MAX_N];
+  for(int i = 0; i < n; i++){
+    itv[i].first = t[i];
+    itv[i].second = s[i];
+  }
+  std::sort(itv, itv+n);
+
   int ans = 0;
   int current = 0;
+
   for(int i = 0; i < n; i++){
-    if(current < s[i]){
-      current = t[i];
+    if(current < itv[i].second){
+      current = itv[i].first;
       ans++;
     }
   }
