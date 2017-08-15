2f9e0151709a44367c6d7d06c7579b07024f7a42
kenta-s <knt01222@gmail.com>
2017-08-16 07:39:25 +0900

    3.cpp

---
+++ ari/nap/201708/3.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+
+const int MAX_N = 100;
+const int MAX_W = 10000;
+
+int n = 4;
+int W = 5;
+int v[] = {3,2,4,2};
+int w[] = {2,1,3,2};
+
+// expected
+// 7
+
+int dp[MAX_N+1][MAX_W+1];
+
+void solve(){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j <= W; j++){
+      if(j < w[i]){
+        dp[i+1][j] = dp[i][j];
+      }else{
+        dp[i+1][j] = std::max(dp[i][j], dp[i][j - w[i]]+v[i]);
+      }
+    }
+  }
+  std::cout << dp[n][W] << std::endl;
+}
+
+int main(){
+  solve();
+  return 0;
+}
