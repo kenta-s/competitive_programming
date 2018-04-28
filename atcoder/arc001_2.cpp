837574dbfe0ee6a0defbb21df9260781e752170e
kenta-s <knt01222@gmail.com>
2018-04-28 13:00:26 +0900

    done

--- atcoder/arc001_2.cpp
+++ atcoder/arc001_2.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
-#include <thread>
+#include <unordered_map>
 
 using namespace std;
 
-void hoge(queue<int> q, int b){
-  int tmp = q.front();
+struct Node{
+  int a;
+  int depth;
+};
+
+unordered_map<int, bool> mp;
+
+void hoge(queue<Node> q, int b){
+  Node node = q.front();
   q.pop();
-  int depth = q.front();
-  q.pop();
-  if(tmp == b){
-    cout << depth << endl;
+  mp[node.a] = true;
+  if(node.a == b){
+    cout << node.depth << endl;
   }else{
-    q.push(tmp + 1);
-    q.push(depth + 1);
-    q.push(tmp - 1);
-    q.push(depth + 1);
-    q.push(tmp + 5);
-    q.push(depth + 1);
-    q.push(tmp - 5);
-    q.push(depth + 1);
-    q.push(tmp + 10);
-    q.push(depth + 1);
-    q.push(tmp - 10);
-    q.push(depth + 1);
+    if(!mp[node.a + 1]){
+      Node plus1 = {node.a + 1, node.depth + 1};
+      q.push(plus1);
+    }
+    if(!mp[node.a - 1]){
+      Node minus1 = {node.a - 1, node.depth + 1};
+      q.push(minus1);
+    }
+    if(!mp[node.a + 5]){
+      Node plus5 = {node.a + 5, node.depth + 1};
+      q.push(plus5);
+    }
+    if(!mp[node.a - 5]){
+      Node minus5 = {node.a - 5, node.depth + 1};
+      q.push(minus5);
+    }
+    if(!mp[node.a + 10]){
+      Node plus10 = {node.a + 10, node.depth + 1};
+      q.push(plus10);
+    }
+    if(!mp[node.a - 10]){
+      Node minus10 = {node.a - 10, node.depth + 1};
+      q.push(minus10);
+    }
     hoge(q, b);
   }
 }
@@ -33,9 +49,10 @@ void hoge(queue<int> q, int b){
 int main(){
   int a, b;
   cin >> a >> b;
-  queue<int> q;
-  q.push(a);
-  q.push(0);
+  queue<Node> q;
+  Node node;
+  node = { a, 0 };
+  q.push(node);
   hoge(q, b);
   return 0;
 }
