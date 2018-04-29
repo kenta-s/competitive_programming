843826e5f0a4d2603ee54484aafb176aa0ec19f0
kenta-s <knt01222@gmail.com>
2018-04-29 19:02:51 +0900

    tmp

--- atcoder/arc001_3.cpp
+++ atcoder/arc001_3.cpp
@@ -5,39 +5,8 @@ using namespace std;
 
 char board[8][9];
 
-bool dfs(int n, int m, int direction1, int direction2){
-  int a, b;
-  a = n+direction1;
-  b = m+direction2;
-
-  if(a < 0 || a > 8 || b < 0 || b > 8) return true;
-  if(board[a][b] == 'Q') return false;
-  board[a][b] = 'x';
-  dfs(a, b, direction1, direction2);
-
+bool dfs(int i, int j){
   return true;
-}
-
-void putQ(){
-  for(int i = 0; i < 8; i++){
-    for(int j = 0; j < 8; j++){
-      if(board[i][j] == '.'){
-        // board[i][j] = 'Q';
-        if(dfs(i, j, -1, -1)
-           && dfs(i, j, -1, 0)
-           && dfs(i, j, -1, 1)
-           && dfs(i, j, 0, -1)
-           && dfs(i, j, 0, 1)
-           && dfs(i, j, 1, -1)
-           && dfs(i, j, 1, 0)
-           && dfs(i, j, 1, 1)){
-        }else{
-          cout << "No Answer" << endl;
-        }
-      }
-    }
-  }
-  
 }
 
 int main(){
@@ -47,18 +16,6 @@ int main(){
   for(int i = 0; i < 8; i++){
     for(int j = 0; j < 8; j++){
       if(board[i][j] == 'Q'){
-        if(dfs(i, j, -1, -1)
-           && dfs(i, j, -1, 0)
-           && dfs(i, j, -1, 1)
-           && dfs(i, j, 0, -1)
-           && dfs(i, j, 0, 1)
-           && dfs(i, j, 1, -1)
-           && dfs(i, j, 1, 0)
-           && dfs(i, j, 1, 1)){
-          // putQ();
-        }else{
-          cout << "No Answer" << endl;
-        }
       }
     }
   }
