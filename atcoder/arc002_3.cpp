ba4a6432ea75ae8a58ea12e99d9309c4b2b94e06
kenta-s <knt01222@gmail.com>
2018-05-03 10:39:24 +0900

    tmp

--- atcoder/arc002_3.cpp
+++ atcoder/arc002_3.cpp
@@ -2,34 +2,47 @@
 
 using namespace std;
 
-
 struct Command{
   int total = 0;
 };
+
+Command AA;
+Command AB;
+Command AX;
+Command AY;
+Command BA;
+Command BB;
+Command BX;
+Command BY;
+Command XA;
+Command XB;
+Command XX;
+Command XY;
+Command YA;
+Command YB;
+Command YX;
+Command YY;
+
+void countup(char a, char b){
+  if(a == 'A' && b == 'A'){
+    cout << "kita" << endl;
+    AA.total = AA.total + 1;
+  }
+}
 
 int main(){
   int N;
   cin >> N;
   char commands[N];
 
-  Command AA;
-  Command AB;
-  Command AX;
-  Command AY;
-  Command BA;
-  Command BB;
-  Command BX;
-  Command BY;
-  Command XA;
-  Command XB;
-  Command XX;
-  Command XY;
-  Command YA;
-  Command YB;
-  Command YX;
-  Command YY;
-
   cin >> commands;
+
+  for(int i = 1; i<=N; i++){
+    countup(commands[i-1], commands[i]);
+    // cout << commands[i-1] + commands[i];
+  }
+  cout << AA.total << endl;
+
   cout << commands << endl;
   return 0;
 }
