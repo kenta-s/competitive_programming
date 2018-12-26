41a14682c9a04963913695450a15797a19a7f961
kenta-s <knt01222@gmail.com>
2018-12-26 23:17:09 +0900

    abc113_c

--- atcoder/abc113/c.rb
+++ atcoder/abc113/c.rb
@@ -1,18 +1,21 @@
 n, m = gets.chomp.split(" ").map(&:to_i)
 cities = []
 
-m.times do
-  cities << gets.split(" ").map(&:to_i)
+m.times do |i|
+  pref, year = gets.split(" ").map(&:to_i)
+  cities << [i, pref, year]
 end
 
-table = {}
-cities.each_with_index do |city, i|
-  table[city[0]] = {} if table[city[0]].nil?
-  table[city[0]] = table[city[0]].merge({city[1] => (table[city[0]].values.max || 0) + 1})
+sorted = cities.sort{|a, b| a[2] <=> b[2]}
+
+h = {}
+sorted.each do |s|
+  num = (h[s[1]] || 0) + 1
+  s << "#{s[1].to_s.rjust(6, '0')}#{num.to_s.rjust(6, '0')}"
+  h[s[1]] = num
 end
 
-a = cities.map do |city|
-  "#{city[0].to_s.rjust(6, '0')}#{table[city[0]][city[1]].to_s.rjust(6, '0')}"
-end
+sorted.sort!{|a,b| a[0] <=> b[0]}
+ans = sorted.map{|s| s[3]}.join("\n")
 
-puts a.join("\n")
+puts ans
