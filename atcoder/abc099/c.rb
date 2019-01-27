8ddeb1b9587ab0205afbdabd11923b0c673ea98d
kenta-s <knt01222@gmail.com>
2019-01-27 21:01:56 +0900

    abc099_c

--- atcoder/abc099/c.rb
+++ atcoder/abc099/c.rb
@@ -1,33 +1,16 @@
 n = gets.to_i
 
-require 'irb'
 nums = [1,6,9,36,81,216,729,1296,6561,7776,46656,59049]
 dp = []
+dp[0] = 0
 
-12.times do |i|
-  dp[i] = []
-  n.times do |j|
-    if i == 0
-      dp[i][j] = j+1
-    else
-      if (j+1) >= nums[i]
-        div = (j+1) / nums[i]
-        mod = (j+1) % nums[i]
-
-        if mod == 0
-          # dp[i][j] = [div, dp[i-1][j]].min
-          dp[i][j] = [div, dp[i-1][j]].min
-        else
-          # dp[i][j] = [(div + dp[i][mod-1]), dp[i-1][j]].min
-          dp[i][j] = [(div + dp[i][mod-1]), dp[i-1][j]].min
-        end
-      else
-        dp[i][j] = dp[i-1][j]
-      end
-    end
+(1..100000).each do |i|
+  dp[i] = Float::INFINITY
+  nums.each do |num|
+    next if i-num < 0
+    next if dp[i-num] == nil
+    dp[i] = [dp[i], 1 + dp[i-num]].min
   end
 end
 
-puts dp[-1][-1]
-
-binding.irb
+puts dp[n]
